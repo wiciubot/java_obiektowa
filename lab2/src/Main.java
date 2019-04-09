c1babec203684838c36a7ac6beeaa2e05aed9904
wiciubot <46847481+wiciubot@users.noreply.github.com>
2019-04-10 00:49:36 +0200

    Add files via upload

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,31 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Analiza Peselu");
         System.out.println("Podaj 11 cyfr swojego peselu");
 
         Scanner scanner = new Scanner(System.in);
+        String pesel=scanner.nextLine();
+        try {
+            long temp = Long.parseLong(pesel);
 
-        String pesel2 = scanner.nextLine();
-        Pesel pesel = new Pesel(pesel2);
-
-        if (pesel.getPesel().length()==11) {
-            String rok=pesel.getPesel().substring(0,4);
-            String mies=pesel.getPesel().substring(5,6);
-            String dzien=pesel.getPesel().substring(7,8);
-            int plec=Integer.parseInt(pesel.getPesel().substring(11));
-            String kontrol=pesel.getPesel().substring(11);
-            if(plec%2) {
-                String plec_wynik ="kobieta";
-            }else {
-                String plec_wynik ="mezczyzna";
-            }
-            System.out.println("Dzien: "+dzien+"\nMiesiac: "+mies+"\nRok: "+rok+"\nPlec: "+plec);
-
-        } else {
-            System.out.println("Podano " + pesel.getPesel().length() + ((pesel.getPesel().length() > 11) ? " za duzo" : " za malo") + " znakow!");
+        }catch (Exception e){
+            System.err.println("Nie podales cyfry!");
+            System.err.println(e.getMessage());
+            System.err.println(e);
         }
+        check(pesel);
+    }
+
+    private static void check(String pesel){
+        if (pesel.length() == 11) {
+            if(control(pesel)==1){
+                Pesel pesel_1 = new Pesel(pesel);
+                System.out.println("Dzien: " + pesel_1.getDzien() + "\nMiesiac: " + pesel_1.getMiesiac() + "\nRok: " + pesel_1.getRok() + "\nPlec: " + pesel_1.getPlec());
+            }else
+                System.err.println("Nie poprawny pesel");
+
+        } else
+            System.err.println("Podano " + pesel.length() + ((pesel.length() > 11) ? " znaki/n za duzo" : " znaki\n za malo") + " znakow");
+    }
+    private static int control(String pesel ) {
+        int wynik=Integer.parseInt(pesel.substring(0,1))*9+Integer.parseInt(pesel.substring(1,2))*7+Integer.parseInt(pesel.substring(2,3))*3+Integer.parseInt(pesel.substring(3,4))*1+Integer.parseInt(pesel.substring(4,5))*9+Integer.parseInt(pesel.substring(5,6))*7+Integer.parseInt(pesel.substring(6,7))*3+Integer.parseInt(pesel.substring(7,8))*1+Integer.parseInt(pesel.substring(8,9))*9+Integer.parseInt(pesel.substring(9,10))*7;
+        if(wynik%10==Integer.parseInt(pesel.substring(10,11))) {
+            return 1;
+        }else
+            return 0;
     }
 }
+
+
+
+
--- lab2/src/Pesel.java
+++ lab2/src/Pesel.java
@@ -1,5 +1,5 @@
 public class Pesel {
-String pesel;
+    String pesel;
 
     Pesel(String pesel){
         this.pesel = pesel;
@@ -8,8 +8,46 @@ String pesel;
     public String getPesel() {
         return pesel;
     }
-
+    public int getRok(){
+        int rok=Integer.parseInt(getPesel().substring(0,2));
+        int miesiac=Integer.parseInt((getPesel().substring(2,4)));
+        if(miesiac>80){
+            return 1800+rok;
+        }else if(miesiac>60){
+            return 2200+rok;
+        }else if(miesiac>40){
+            return 2100+rok;
+        }else if(miesiac>20){
+            return 2000+rok;
+        }else
+            return 1900+rok;
+    }
+    public int getMiesiac(){
+        int miesiac=Integer.parseInt(getPesel().substring(2,4));
+        if(miesiac>80){
+            return miesiac-80;
+        }else if(miesiac>60){
+            return miesiac-60;
+        }else if(miesiac>40){
+            return miesiac-40;
+        }else if(miesiac>20){
+            return miesiac-20;
+        }else
+            return miesiac;
+    }
+    public int getDzien(){
+        int dzien=Integer.parseInt(getPesel().substring(4,6));
+        return dzien;
+    }
+    public String getPlec(){
+        int plec=Integer.parseInt(getPesel().substring(9,10));
+        if(plec%2==0) {
+            return "kobieta";
+        }else
+            return "mezczyzna";
+    }
     public void setPesel(String pesel) {
         this.pesel = pesel;
     }
+
 }
